fcfa6b6142c5b6ef0de4b6097e4d11cc9dc6a6b1
Mitch Allen <mitch.c.allen@gmail.com>
2016-07-02 14:33:36 -0700

    Updated demo

--- examples/demo/config.js
+++ examples/demo/config.js
@@ -5,4 +5,6 @@ config.endpoint = "tcp://localhost:5431";
 
 config.interval = 3000;    // micro seconds
 
+config.verbose = true;
+
 module.exports = config; 
---
+++ examples/demo/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var ZmqJsonSub = require('../../zmq-json-sub'),
+    config = require('./config'),
+    sub = null;
+
+var spec = {
+    endpoint: config.endpoint, 
+    filter: "", 
+    onMessage: function (data) {
+        let jstring = JSON.parse(data);
+        console.log( jstring );
+        if (jstring.timestamp) {
+            console.log( "TIME STAMP: [" 
+                + new Date(jstring.timestamp) 
+                + "]");
+        }
+    }
+};
+
+sub = new ZmqJsonSub(spec, function(err, data) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+});
+
+console.log("Subscribed to: " + config.endpoint);
+
+/*
+    Handle termination - close sub.
+*/
+
+//  terminator === the termination handler.
+function terminator(sig) {
+   if (typeof sig === "string") {
+      console.log('%s: Received %s - terminating Node server ...',
+                  Date(Date.now()), sig);
+      console.log("Closing subcriber before exit");
+      sub.close();
+      process.exit(1);
+   }
+   console.log('%s: Node server stopped.', Date(Date.now()) );
+}
+
+//  Process on exit and signals.
+process.on('exit', function() { terminator(); });
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
+ 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
+].forEach(function(element, index, array) {
+    process.on(element, function() { terminator(element); });
+});
+
+
